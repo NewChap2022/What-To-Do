0141b1ccc58d37dd5d09e24d46f32e76c2167713
NewChap2022 <irisyangshu@gmail.com>
2022-11-25 11:41:30 -0500

    fix deployment bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "bootswatch/dist/sketchy/bootstrap.min.css"
 import './App.css';
 import React from 'react';
-import { BrowserRouter, Router, Routes, Route } from 'react-router-dom';
+import { Routes, Route, HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './utils/store';
 
@@ -10,23 +10,19 @@ import List from "./pages/List";
 
 export default function App() {
   return (
-    <BrowserRouter baseName={process.env.PUBLIC_URL}>
-      <Router>
+      <HashRouter>
         <Provider store={store}>
           <Routes>
             <Route
-              exact
               path="/"
               element={<Home />}
             />
             <Route
-              exact
               path="/list"
               element={<List />}
             />
           </Routes>
         </Provider>
-      </Router>
-    </BrowserRouter>
+      </HashRouter>
   );
 }
